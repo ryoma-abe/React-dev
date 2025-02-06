fc447c24d0b3ca2763cd0119d823b8a84b942066
ryoma-abe <liaozhenanbu387@gmail.com>
2025-02-07 06:20:08 +0900

    デフォルト値README追加

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const sayHello = (name = "じゃけえ") => console.log(`私の名前は${name}です。`);
 sayHello();
+
+const myProfile = {
+  name: "安部",
+};
+
+const { name, age = 21 } = myProfile;
+console.log(name);
+console.log(age);
