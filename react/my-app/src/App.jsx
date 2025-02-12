f9fe4291bb508c568f8a91bb3bbf184ecf35ef88
ryoma-abe <liaozhenanbu387@gmail.com>
2025-02-12 21:31:24 +0900

    Stateを知る

--- react/my-app/src/App.jsx
+++ react/my-app/src/App.jsx
@@ -4,7 +4,7 @@ import { ColorfulMessage } from "./components/ColorfulMessage";
 export const App = () => {
   const [num, setNum] = useState(0);
   const onClickButton = () => {
-    alert("ボタン");
+    setNum(num + 1);
   };
 
   return (
@@ -12,6 +12,7 @@ export const App = () => {
       <h1>こんにちは！</h1>
       <ColorfulMessage color="blue" message="元気です" />
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 };
