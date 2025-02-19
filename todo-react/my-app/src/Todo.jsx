cc57f0c01e8397727d43e630427992dd096f7e36
ryoma-abe <liaozhenanbu387@gmail.com>
2025-02-20 07:13:04 +0900

    mapで配列ループ

--- todo-react/my-app/src/Todo.jsx
+++ todo-react/my-app/src/Todo.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import "./Todo.css";
 
 export const Todo = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "TODOです1",
+    "TODOです2",
+  ]);
   return (
     <div className="container">
       {/* 入力エリア */}
@@ -15,16 +20,15 @@ export const Todo = () => {
       <div className="incomplete-area">
         <h2>未完了のTODO</h2>
         <ul id="todo-list" className="todo-list">
-          <li className="todo-item">
-            <span>TODOです</span>
-            <button className="complete-button">完了</button>
-            <button className="delete-button">削除</button>
-          </li>
-          <li className="todo-item">
-            <span>TODOです</span>
-            <button className="complete-button">完了</button>
-            <button className="delete-button">削除</button>
-          </li>
+          {incompleteTodos.map((todo, index) => {
+            return (
+              <li key={index} className="todo-item">
+                <span>{todo}</span>
+                <button className="complete-button">完了</button>
+                <button className="delete-button">削除</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
 
