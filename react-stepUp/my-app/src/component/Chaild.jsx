9676561860f48511f8a0693fc8ab84c88e876be4
ryoma-abe <liaozhenanbu387@gmail.com>
2025-02-23 09:21:58 +0900

    CSSについてまとめ

--- react-stepUp/my-app/src/component/Chaild.jsx
+++ react-stepUp/my-app/src/component/Chaild.jsx
@@ -1,7 +1,9 @@
 export const Chaild = ({ open }) => {
   return (
     <>
-      <div>{open && <p className="mt-10 backdrop-blur-lg">子コンポーネント</p>}</div>
+      <div>
+        {open && <p className="mt-10 backdrop-blur-lg">子コンポーネント</p>}
+      </div>
     </>
   );
 };
