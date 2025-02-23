c1ef360c70a8637d70dd5385b7a75c43db7249c2
ryoma-abe <liaozhenanbu387@gmail.com>
2025-02-23 09:54:03 +0900

    styledJSX

--- react-stepUp/my-app/src/component/Chaild.jsx
+++ react-stepUp/my-app/src/component/Chaild.jsx
@@ -2,8 +2,18 @@ export const Chaild = ({ open }) => {
   return (
     <>
       <div>
-        {open && <p className="mt-10 backdrop-blur-lg">子コンポーネント</p>}
+        {open && (
+          <p className="mt-10 backdrop-blur-lg text-child">子コンポーネント</p>
+        )}
       </div>
+      <style jsx="true">
+        {`
+          .text-child {
+            font-size: 100px;
+            margin: 100px;
+          }
+        `}
+      </style>
     </>
   );
 };
