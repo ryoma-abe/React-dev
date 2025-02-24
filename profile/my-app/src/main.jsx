601e0ee50b044c4448c25cc839d68f64bc860191
ryoma-abe <liaozhenanbu387@gmail.com>
2025-02-25 07:13:25 +0900

    About追加

--- profile/my-app/src/main.jsx
+++ profile/my-app/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Header } from "./components/Header";
+import { FirstView } from "./components/FirstView";
+import { About } from "./components/About";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Header />
+    <FirstView />
+    <About />
   </StrictMode>
 );
