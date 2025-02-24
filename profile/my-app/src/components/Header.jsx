63b0bccf2049092d1dc0e7763745e0f3cd3a605d
ryoma-abe <liaozhenanbu387@gmail.com>
2025-02-25 06:13:10 +0900

    header追加

---
+++ profile/my-app/src/components/Header.jsx
@@ -0,0 +1,19 @@
+export const Header = () => {
+  return (
+    <header>
+      <div>
+        <img src="/src/assets/logo.svg" alt="ロゴ画像" />
+        <nav>
+          <ul>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Bicycle</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
