37dc502b120078abf1749b441c4a6fa90c86e4f7
ryoma-abe <liaozhenanbu387@gmail.com>
2025-02-25 07:22:02 +0900

    React発展MD作成

---
+++ profile/my-app/src/pages/PageAbout.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header";
+
+export const PageAbout = () => {
+  return (
+    <>
+      <div>
+        <Header />
+        <h1>ここはアバウトページです。</h1>
+      </div>
+    </>
+  );
+};
