9e47d1e1c601ccc86c87df17d101c644fb9b6794
ryoma-abe <liaozhenanbu387@gmail.com>
2025-02-27 08:03:59 +0900

    molecules追加

--- Atomic/my-app/src/App.jsx
+++ Atomic/my-app/src/App.jsx
@@ -1,11 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
 
 export const App = () => {
+  const WrapperStyle = {
+    maxWidth: "800px",
+    marginInline: "auto",
+  };
   return (
-    <>
+    <div style={WrapperStyle}>
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
-    </>
+      <br />
+      <br />
+      <SearchInput />
+    </div>
   );
 };
--- Atomic/my-app/src/components/atoms/button/SecondaryButton.jsx
+++ Atomic/my-app/src/components/atoms/button/SecondaryButton.jsx
@@ -5,15 +5,5 @@ export const SecondaryButton = ({ children }) => {
   const Button = styled(BaseButton)`
     background: #11999e;
   `;
-  const ButtonWrapper = styled.button`
-    width: 100%;
-    text-align: center;
-    background-color: #fff;
-    border: none;
-  `;
-  return (
-    <ButtonWrapper>
-      <Button>{children}</Button>
-    </ButtonWrapper>
-  );
+  return <Button>{children}</Button>;
 };
