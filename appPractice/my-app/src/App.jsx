abebca84a9f70b25603e2c46649da34015ed79db
ryoma-abe <liaozhenanbu387@gmail.com>
2025-03-02 09:42:58 +0900

    シンプルカウントアップ追加

--- appPractice/my-app/src/App.jsx
+++ appPractice/my-app/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [countUp, setCountUp] = useState(0);
+  const onClickAdd = () => {
+    setCountUp(countUp + 1);
+  };
+  const onClickPrev = () => {
+    setCountUp(countUp - 1);
+  };
+  const onClickReset = () => {
+    setCountUp(0);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>{countUp}</p>
+      <button onClick={onClickAdd}>Add</button>
+      <button onClick={onClickPrev}>Prev</button>
+      <button onClick={onClickReset}>Reset</button>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
