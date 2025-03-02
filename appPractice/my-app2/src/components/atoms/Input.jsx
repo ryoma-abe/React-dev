372e6f62221f543997d00c29116f96d2a3f5ad1d
ryoma-abe <liaozhenanbu387@gmail.com>
2025-03-03 08:03:42 +0900

    インプットテキストの動作追加

--- appPractice/my-app2/src/components/atoms/Input.jsx
+++ appPractice/my-app2/src/components/atoms/Input.jsx
@@ -1,3 +1,17 @@
+import { useState } from "react";
+
 export const Input = ({ placeholder }) => {
-  return <input className="border p-2 w-full" type="text" placeholder={placeholder} />;
+  const [inputText, setInputText] = useState("テキスト");
+  const onChangeInputText = (e) => {
+    setInputText.apply(e.target.value);
+  };
+  return (
+    <input
+      className="border p-2 w-full"
+      type="text"
+      placeholder={placeholder}
+      value={inputText}
+      onChange={onChangeInputText}
+    />
+  );
 };
