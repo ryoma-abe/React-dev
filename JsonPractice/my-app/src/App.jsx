9b12a6e28dbafac7443eec7c7d811f465cbc6481
ryoma-abe <liaozhenanbu387@gmail.com>
2025-03-06 06:26:47 +0900

    axios

--- JsonPractice/my-app/src/App.jsx
+++ JsonPractice/my-app/src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const onClickUsers = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((r) => {
+      console.log(r.data);
+    });
+  };
+  const onClickUser1 = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users?id=1").then((r) => {
+      console.log(r.data);
+    });
+  };
   return (
     <>
-      <button>users</button>
-      <button>id=1</button>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1</button>
     </>
   );
 }
