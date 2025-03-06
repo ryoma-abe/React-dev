f0d55be1870570b12e9533be50e621528433cc37
ryoma-abe <liaozhenanbu387@gmail.com>
2025-03-07 06:50:33 +0900

    型無し実装

--- TsPractice/my-app/src/practices/Practice1.tsx
+++ TsPractice/my-app/src/practices/Practice1.tsx
@@ -1,14 +1,21 @@
 import axios from "axios";
+import { useState } from "react";
+import { Todo } from "../Todo";
+
 
 export const Practice1 = () => {
+  const [todos, setTodos] = useState<any>([]);
   const onClickFetchDate = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((r) => {
-      console.log(r.data);
+      setTodos(r.data);
     });
   };
   return (
     <>
       <button onClick={onClickFetchDate}>データを取得</button>
+      {todos.map((todo) => (
+        <Todo/>
+      ))}
     </>
   );
 };
