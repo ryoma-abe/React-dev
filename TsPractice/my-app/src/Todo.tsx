f18433f6f3f6d957b33132ccc275d06955841c60
ryoma-abe <liaozhenanbu387@gmail.com>
2025-03-09 07:37:25 +0900

    オプショナルチェイニング

--- TsPractice/my-app/src/Todo.tsx
+++ TsPractice/my-app/src/Todo.tsx
@@ -1,10 +1,11 @@
+import { FC } from "react";
 import { TodoType } from "./types/todo";
 
-export const Todo = ({
+export const Todo: FC<Omit<TodoType, "id">> = ({
   title,
   userId,
   completed = false,
-}: Omit<TodoType, "id">) => {
+}) => {
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(id:${userId})`}</p>;
 };
