e540d39c33cec275b6abc7811fc1986f1c0605a8
ryoma-abe <liaozhenanbu387@gmail.com>
2025-03-10 07:03:27 +0900

    カードデータ取得まで実装

--- CustomFook/my-app/src/App.tsx
+++ CustomFook/my-app/src/App.tsx
@@ -1,5 +1,9 @@
+import axios from "axios";
 import "./App.css";
 import { UserCard } from "./components/UserCard";
+import { User } from "./types/api/user";
+import { useState } from "react";
+import { UserProfile } from "./types/userProfile";
 
 const user = {
   id: 1,
@@ -9,9 +13,28 @@ const user = {
 };
 
 function App() {
+  const [userProfile, setUserProfile] = useState<Array<UserProfile>>([]);
+  const onClickUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((r) => {
+        const data = r.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`,
+        }));
+        setUserProfile(data);
+      });
+  };
   return (
     <>
-      <UserCard user={user} />
+      <button onClick={onClickUser} className="border bg-blue-400 p-10 m-10">
+        データ取得
+      </button>
+      {userProfile.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </>
   );
 }
