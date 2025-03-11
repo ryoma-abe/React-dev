35c0bb739ccd56920681c18d3595fce0570fc12b
ryoma-abe <liaozhenanbu387@gmail.com>
2025-03-12 07:10:54 +0900

    ネストルーティング追加

--- graduation/my-app/src/components/Header.tsx
+++ graduation/my-app/src/components/Header.tsx
@@ -4,10 +4,10 @@ export const Header = () => {
   return (
     <header className="bg-gray-100 p-4 shadow-md">
       <nav className="flex gap-4 justify-center">
-        <Link to="/" className="px-3 py-2 hover:bg-gray-200 rounded">ホーム</Link>
-        <Link to="/login" className="px-3 py-2 hover:bg-gray-200 rounded">ログイン</Link>
-        <Link to="/setting" className="px-3 py-2 hover:bg-gray-200 rounded">設定</Link>
-        <Link to="/userManagement" className="px-3 py-2 hover:bg-gray-200 rounded">ユーザー</Link>
+        <Link to="/" className="px-3 py-2 hover:bg-gray-200 rounded">ログイン</Link>
+        <Link to="/home" className="px-3 py-2 hover:bg-gray-200 rounded">ホーム</Link>
+        <Link to="/home/setting" className="px-3 py-2 hover:bg-gray-200 rounded">設定</Link>
+        <Link to="/home/userManagement" className="px-3 py-2 hover:bg-gray-200 rounded">ユーザー</Link>
       </nav>
     </header>
   );
--- graduation/my-app/src/router/Router.tsx
+++ graduation/my-app/src/router/Router.tsx
@@ -7,10 +7,11 @@ import { UserManagement } from "../components/pages/UserManagement";
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/setting" element={<Setting />} />
-      <Route path="/userManagement" element={<UserManagement />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />}>
+        <Route path="setting" element={<Setting />} />
+        <Route path="userManagement" element={<UserManagement />} />
+      </Route>
     </Routes>
   );
 };
