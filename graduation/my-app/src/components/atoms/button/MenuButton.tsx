6eec9552d2674391ea348c0e258f179e2737d8ec
ryoma-abe <liaozhenanbu387@gmail.com>
2025-03-14 06:19:48 +0900

    headernav修正

--- graduation/my-app/src/components/atoms/button/MenuButton.tsx
+++ graduation/my-app/src/components/atoms/button/MenuButton.tsx
@@ -1,43 +1,48 @@
 import { FC } from "react";
-import { MenuButtonType } from "../../../types/MenuButton";
+
+type MenuButtonType = {
+  toggleMenu: () => void;
+  menuOpen: boolean;
+};
 
 export const MenuButton: FC<MenuButtonType> = ({ toggleMenu, menuOpen }) => {
   return (
-    <div className="flex justify-between items-center md:hidden">
-      <button onClick={toggleMenu} className="p-2 rounded hover:bg-gray-200">
-        {menuOpen ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <line x1="18" y1="6" x2="6" y2="18"></line>
-            <line x1="6" y1="6" x2="18" y2="18"></line>
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <line x1="3" y1="12" x2="21" y2="12"></line>
-            <line x1="3" y1="6" x2="21" y2="6"></line>
-            <line x1="3" y1="18" x2="21" y2="18"></line>
-          </svg>
-        )}
-      </button>
-    </div>
+    <button
+      onClick={toggleMenu}
+      className="p-2 rounded hover:bg-gray-200 md:hidden"
+    >
+      {menuOpen ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <line x1="18" y1="6" x2="6" y2="18"></line>
+          <line x1="6" y1="6" x2="18" y2="18"></line>
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <line x1="3" y1="12" x2="21" y2="12"></line>
+          <line x1="3" y1="6" x2="21" y2="6"></line>
+          <line x1="3" y1="18" x2="21" y2="18"></line>
+        </svg>
+      )}
+    </button>
   );
 };
