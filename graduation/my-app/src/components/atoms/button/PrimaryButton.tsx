2e2b3c872027b7b1848a36e6d182b5cbfc133e37
ryoma-abe <liaozhenanbu387@gmail.com>
2025-03-14 07:27:58 +0900

    ログイン画面型追加

--- graduation/my-app/src/components/atoms/button/PrimaryButton.tsx
+++ graduation/my-app/src/components/atoms/button/PrimaryButton.tsx
@@ -1,4 +1,10 @@
-export const PrimaryButton = ({ children }) => {
+import { FC, ReactNode } from "react";
+
+type PrimaryButtonType = {
+  children: ReactNode;
+};
+
+export const PrimaryButton: FC<PrimaryButtonType> = ({ children }) => {
   return (
     <button className="w-full px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-colors">
       {children}
