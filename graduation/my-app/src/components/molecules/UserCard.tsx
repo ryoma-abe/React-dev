b044e0749b440444737e84829491f20113ffcc23
ryoma-abe <liaozhenanbu387@gmail.com>
2025-03-20 06:22:49 +0900

    モーダルコンポーネント分割

--- graduation/my-app/src/components/molecules/UserCard.tsx
+++ graduation/my-app/src/components/molecules/UserCard.tsx
@@ -1,6 +1,5 @@
-import { FC, useEffect } from "react";
-import { useAllUsers } from "../../hooks/useAllUsers";
-import { Spinner } from "../atoms/Spinner";
+import { FC } from "react";
+
 type userCardProps = {
   id: number;
   imgUrl: string;
@@ -16,35 +15,21 @@ export const UserCard: FC<userCardProps> = ({
   fullName,
   onClick,
 }) => {
-  const { getUsers, loading, users } = useAllUsers();
-  useEffect(() => getUsers(), []);
-
   return (
     <>
-      {loading ? (
-        <div className="flex items-center justify-center w-full h-screen">
-          <Spinner />
-        </div>
-      ) : (
-        <div className="grid lg:grid-cols-3 gap-2 md:grid-cols-2 grid-cols-1">
-          {users?.map((user) => (
-            <div
-              onClick={() => onClick(id)}
-              className="bg-white p-4 rounded shadow-md"
-            >
-              <img
-                src={imgUrl}
-                alt={user.name}
-                width="300px"
-                height="300px"
-                className="w-30 h-30 rounded-full mx-auto mb-2"
-              />
-              <h3 className="text-lg font-semibold text-center">{userName}</h3>
-              <p className="text-gray-600 text-center">{fullName}</p>
-            </div>
-          ))}
-        </div>
-      )}
+      <div
+        onClick={() => onClick(id)}
+        className="bg-white p-4 rounded shadow-md"
+      >
+        <img
+          src={imgUrl}
+          width="300px"
+          height="300px"
+          className="w-30 h-30 rounded-full mx-auto mb-2"
+        />
+        <h3 className="text-lg font-semibold text-center">{userName}</h3>
+        <p className="text-gray-600 text-center">{fullName}</p>
+      </div>
     </>
   );
 };
--- graduation/my-app/src/components/pages/UserManagement.tsx
+++ graduation/my-app/src/components/pages/UserManagement.tsx
@@ -1,21 +1,38 @@
-import { FC, memo, useState } from "react";
+import { FC, memo, useEffect, useState } from "react";
 import { UserCard } from "../molecules/UserCard";
 import { Modal } from "../atoms/Modal";
+import { Spinner } from "../atoms/Spinner";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
+  const { loading, users, getUsers } = useAllUsers();
+  console.log(users);
+
   const [modal, setModal] = useState(false);
   const modalToggle = () => setModal(!modal);
+
+  useEffect(() => getUsers(), []);
   return (
     <>
-      <div className="p-4">
-        <UserCard
-          id={1}
-          imgUrl="https://picsum.photos/id/11/500/350"
-          userName={"テスト"}
-          fullName="テスト・テスト"
-          onClick={modalToggle}
-        />
-      </div>
+      {loading ? (
+        <div className="flex items-center justify-center w-full h-screen">
+          <Spinner />
+        </div>
+      ) : (
+        <div className="grid lg:grid-cols-3 gap-2 md:grid-cols-2 grid-cols-1">
+          {users?.map((user) => (
+            <div className="p-4">
+              <UserCard
+                id={1}
+                imgUrl="https://picsum.photos/id/11/500/350"
+                userName={user.name}
+                fullName={user.name}
+                onClick={modalToggle}
+              />
+            </div>
+          ))}
+        </div>
+      )}
       {modal && <Modal modalToggle={modalToggle} />}
     </>
   );
