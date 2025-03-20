4ab7c3afdb6ecf15eb5978c5f9fa9692e88ef361
ryoma-abe <liaozhenanbu387@gmail.com>
2025-03-21 06:00:21 +0900

    ログイン状態のフックを追加

--- graduation/my-app/src/router/Router.tsx
+++ graduation/my-app/src/router/Router.tsx
@@ -4,16 +4,19 @@ import { Login } from "../components/pages/Login";
 import { Setting } from "../components/pages/Setting";
 import { UserManagement } from "../components/pages/UserManagement";
 import { Page404 } from "../components/pages/Page404";
+import { LoginUserProvider } from "../providers/LoginUserProvider";
 
 export const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />}>
-        <Route path="setting" element={<Setting />} />
-        <Route path="userManagement" element={<UserManagement />} />
-      </Route>
-      <Route path="*" element={<Page404 />} />
-    </Routes>
+    <LoginUserProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />}>
+          <Route path="setting" element={<Setting />} />
+          <Route path="userManagement" element={<UserManagement />} />
+        </Route>
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </LoginUserProvider>
   );
 };
